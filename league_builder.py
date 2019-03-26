1e3550dbbf12d2a0836a4f984f8a40fce29e889f
OXDavidXO <44665014+OXDavidXO@users.noreply.github.com>
2019-03-26 00:15:01 -0500

    (UPDATE) league_builder.py
    Updated  and re-written all code, added new options, made sure txt file was was being updated when run and not duplicating itself when program was executed again, made sure program was clean and readable.

--- league_builder.py
+++ league_builder.py
@@ -2,119 +2,197 @@ if __name__ == "__main__":
     import csv
     import sys
 
-    def teams_summary():
-        with open('teams.txt', 'a') as file:
-            file.write("Sharks\n"
-                       "Joe Smith, YES, Jim and Jan Smith.\n"
-                       "Jill Tanner, YES, Clara Tanner.\n"
-                       "Bill Bon, YES, Sara and Jenny Bon.\n"
-                       "Eva Gordon, NO, Wendy and Mike Gordon.\n"
-                       "Mat Gill, NO, Charles and Sylvia Gill.\n"
-                       "Kimmy Stein, NO, Billy and Hillary Stein.\n\n"
-
-                       "Dragons\n"
-                       "Karl Saygan, YES, Heather Bledsoe.\n"
-                       "Suzane Greenburg, YES, Henrietta Dumas.\n"
-                       "Diego Soto, YES, Robin and Sarika Soto.\n"
-                       "Sammy Adams, NO, Jeff Adams.\n"
-                       "Sal Dali, NO, Gala Dali.\n"
-                       "Joe Kavalier, NO, Sam and Elaine Kavalier.\n\n"
-
-                       "Raptors\n"
-                       "Philip Helm, YES, Thomas Helm and Eva Jones.\n"
-                       "Les Clay, YES, Wynonna Brown.\n"
-                       "Herschel Krustofski, YES, Hyman and Rachel Krustofski.\n"
-                       "Ben Finkelstein, NO, Aaron and Jill Finkelstein.\n"
-                       "Chloe Alaska, NO, David and Jamie Alaska.\n"
-                       "Arnold Willis, NO, Claire Willis.")
-
-    def player_info():
-        with open('soccer_players.csv') as csvfile:
-
-            player_info = csv.reader(csvfile)
-            for row in player_info:
-
-                print(row)
-
-    def team_sharks():
-        experianced_players = {'name_1': 'Joe Smith',
-                               'name_2': 'Jill Tanner',
-                               'name_3': 'Bill Bon'}
-
-        non_experianced_players = {'name_1': 'Eva Gordon',
-                                   'name_2': 'Mat Gill',
-                                   'name_3': 'Kimmy Stein'}
-        sharks = []
-
-        for name, names in experianced_players.items():
-            sharks.append(names)
-
-        for name, names in non_experianced_players.items():
-            sharks.append(names)
-
-        print("Team Sharks: {}\n".format(sharks))
-
-    def team_dragons():
-        experianced_players = {'name_1': 'Karl Saygan',
-                               'name_2': 'Suzane Greenburg',
-                               'name_3': 'Diego Soto'}
-
-        non_experianced_players = {'name_1': 'Sammy Adams',
-                                   'name_2': 'Sal Dali',
-                                   'name_3': 'Joe Kavalier'}
-        dragons = []
-
-        for name, names in experianced_players.items():
-            dragons.append(names)
-
-        for name, names in non_experianced_players.items():
-            dragons.append(names)
-
-        print("Team Dragons: {}\n".format(dragons))
-
-    def team_raptors():
-        experianced_players = {'name_1': 'Philip Helm',
-                               'name_2': 'Les Clay',
-                               'name_3': 'Herschel Krustofski'}
-
-        non_experianced_players = {'name_1': 'Ben Finkelstein',
-                                   'name_2': 'Chloe Alaska',
-                                   'name_3': 'Arnold Willis'}
-        raptors = []
-
-        for name, names in experianced_players.items():
-            raptors.append(names)
-
-        for name, names in non_experianced_players.items():
-            raptors.append(names)
-
-        print("Team Raptors: {}\n".format(raptors))
-
-    def user_question():
-        print("Type in 'B' to build the teams!\n")
-        print("Type in 'I' to get player information!\n")
-        print("Type in 'Q' to Quit!\n")
+    def user_interaction():
+        print("What would you like to do?\n")
+        print("Type 'B' to build the teams!\n")
+        print("Type 'E' to see team experience!\n")
+        print("type 'P' to see player parents!\n")
+        print("Type 'I' to see player information!\n")
+        print("Type 'Q' to quit the program!\n")
 
         while True:
-            user_answer = input("What would you like to do?: \n")
-
-            if user_answer.lower() == 'b':
-                team_sharks()
-                team_dragons()
-                team_raptors()
-                break
-
-            elif user_answer.lower() == 'i':
+            user_options = input("Choose an option: ")
+            if user_options.lower() == 'b':
+                team_builder()
+                continue
+            elif user_options.lower() == 'e':
+                soccer_experience()
+                continue
+            elif user_options.lower() == 'p':
+                parents()
+                continue
+            elif user_options.lower() == 'i':
                 player_info()
                 continue
+            elif user_options.lower() == 'q':
+                sys.exit("Thanks for the parcipitation!!")
 
-            elif user_answer.lower() == 'q':
-                sys.exit("Thanks for the precipitation!")
+    def program_start():
+        print("Hello and welcome to League Builder!\n")
+        print("Today we will create three teams called the:\n")
+        print("Sharks, Dragons and the Raptors!\n")
 
-    print("Hello and welcome to League Builder!\n")
+        while True:
+            user_start = input("Press 'S' to start!: ")
+            if user_start.lower() == 's':
+                user_interaction()
 
-    teams_summary()
-    user_question()
+    def team_builder():
 
-    print("Here are the teams!")
-    print("Have a nice day!")
+        players = []
+        sharks = []
+        dragons = []
+        raptors = []
+
+        with open('soccer_players.csv', 'r') as csvfile:
+            csv_file = csv.reader(csvfile)
+
+            next(csv_file)
+            for line in csv_file:
+                players.append(line[0])
+
+            sharks.append(players[0:6])
+            print("Team Sharks: {} \n".format(sharks))
+            del players[0:6]
+
+            dragons.append(players[0:5])
+            dragons.append(players[6])
+            print("Team Dragons: {} \n".format(dragons))
+            del players[0:5]
+            del players[1]
+
+            raptors.append(players[0:6])
+            print("Team Raptors: {} \n".format(raptors))
+            del players[0:6]
+
+    def soccer_experience():
+
+        exp_players = []
+        sharks = []
+        dragons = []
+        raptors = []
+
+        with open('soccer_players.csv', 'r') as csvfile:
+            csv_file = csv.reader(csvfile)
+
+            next(csv_file)
+            for line in csv_file:
+                exp_players.append(line[2])
+
+            sharks.append(exp_players[0:6])
+            print("Experience for Team Sharks: {} \n".format(sharks))
+            del exp_players[0:6]
+
+            dragons.append(exp_players[0:5])
+            dragons.append(exp_players[6])
+            print("Experience for Team Dragons: {} \n".format(dragons))
+            del exp_players[0:5]
+            del exp_players[1]
+
+            raptors.append(exp_players[0:6])
+            print("Experience for Team Raptors: {} \n".format(raptors))
+            del exp_players[0:6]
+
+    def parents():
+        gaurdians = []
+        sharks_parents = []
+        dragons_parents = []
+        raptors_parents = []
+        with open('soccer_players.csv', 'r') as csvfile:
+            csv_file = csv.reader(csvfile)
+
+            next(csv_file)
+            for line in csv_file:
+                gaurdians.append(line[3])
+
+            sharks_parents.append(gaurdians[0:6])
+
+            print("Parent names for Team Sharks: {} \n"
+                  .format(sharks_parents))
+
+            del gaurdians[0:6]
+
+            dragons_parents.append(gaurdians[0:5])
+            dragons_parents.append(gaurdians[6])
+
+            print("Parent names for Team Dragons: {} \n"
+                  .format(dragons_parents))
+
+            del gaurdians[0:5]
+            del gaurdians[1]
+
+            raptors_parents.append(gaurdians[0:6])
+
+            print("Parent names for Team Raptors: {} \n"
+                  .format(raptors_parents))
+
+            del gaurdians[0:6]
+
+    def player_info():
+        with open('soccer_players.csv', 'r') as csvfile:
+            csv_file = csv.reader(csvfile)
+            for line in csv_file:
+                print(line)
+
+    def team_file():
+        with open('teams.txt', 'w') as txtfile:
+            team_names = csv.writer(txtfile)
+            fieldnames = ['Name', 'Soccer Experience', 'Gaurdian Name(s)']
+            writer = csv.DictWriter(txtfile, fieldnames=fieldnames)
+
+            team_names.writerow(['Sharks'])
+            writer.writerow({'Name': 'Joe Smith', 'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Jim and Jan Smith'})
+            writer.writerow({'Name': 'Jill Tanner', 'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Clara Tanner'})
+            writer.writerow({'Name': 'Bill Bon', 'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Sara and Jenny Bon'})
+
+            writer.writerow({'Name': 'Eva Gordan', 'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Wendy and Mike Gordon'})
+            writer.writerow({'Name': 'Matt Gill', 'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Charles and Sylvia Gill'})
+            writer.writerow({'Name': 'Kimmy Stein', 'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Bill and Hillary Stein'})
+
+            team_names.writerow([])
+
+            team_names.writerow(['Dragons'])
+            writer.writerow({'Name': 'Sammy Adams', 'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Jeff Adams'})
+            writer.writerow({'Name': 'Karl Saygan', 'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Heather Bledsoe'})
+            writer.writerow({'Name': 'Suzane Greenberg',
+                             'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Henrietta Dumas'})
+
+            writer.writerow({'Name': 'Sal Dali', 'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Gala Dali'})
+            writer.writerow({'Name': 'Joe Kavalier', 'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Sam and Elaine Kavalier'})
+            writer.writerow({'Name': 'Diego Soto', 'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Robin and Sarika Soto'})
+
+            team_names.writerow([])
+
+            team_names.writerow(['Raptors'])
+            writer.writerow({'Name': 'Ben Finkelstein',
+                             'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Aaron and Jill Finkelstein'})
+            writer.writerow({'Name': 'Chloe Alaska', 'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'David and Jamie Alaska'})
+            writer.writerow({'Name': 'Arnold Willis',
+                             'Soccer Experience': 'NO',
+                             'Gaurdian Name(s)': 'Claire Willis'})
+
+            writer.writerow({'Name': 'Philip Helm', 'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Thomas Helm and Eva Jones'})
+            writer.writerow({'Name': 'Les Clay', 'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)': 'Wynonna Brown'})
+            writer.writerow({'Name': 'Herschel Krustofski',
+                             'Soccer Experience': 'YES',
+                             'Gaurdian Name(s)':
+                             'Hyman and Rachel Krustofski'})
+
+    team_file()
+    program_start()
